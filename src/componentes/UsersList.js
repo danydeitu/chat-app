d715c3b1bb5d108f81b79bee51fd169f1b06674d
Daniel <danydeitu18@gmail.com>
2023-11-22 16:04:56 -0300

    elaboro los componentes

--- src/componentes/UsersList.js
+++ src/componentes/UsersList.js
@@ -1,16 +1,15 @@
-// UsersList.js
 import React, { useState, useEffect } from 'react';
 
 const UsersList = () => {
   const [users, setUsers] = useState([]);
 
-  // Lógica para obtener y mostrar la lista de usuarios
+  // Lógica lista de usuarios
 
   return (
     <div>
       <h2>Usuarios</h2>
       <ul>
-        {/* Renderizar la lista de usuarios aquí */}
+        /* Renderizar la lista */
       </ul>
     </div>
   );
